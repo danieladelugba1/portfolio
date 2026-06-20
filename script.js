d9f3d94492ab4be6634a04e7a560f59014ceb68d
Daniel Adelugba <psyda7@nottingham.ac.uk>
2026-06-20 16:38:16 +0100

    feat: Initial personal detai added
    Features added include; sepeare cards for project, flex-bbox stylign added for strucutre of page, navigation links for specific sections of web page

---
+++ script.js
@@ -0,0 +1,15 @@
+// const themeBtn = document.getElementById("themeBtn");
+
+// themeBtn.addEventListener("click", () => {
+
+//     document.body.classList.toggle("dark-mode");
+
+//     const darkModeEnabled =
+//         document.body.classList.contains("dark-mode");
+
+//     localStorage.setItem("darkMode", darkModeEnabled);
+// });
+
+// if (localStorage.getItem("darkMode") === "true") {
+//     document.body.classList.add("dark-mode");
+// }
