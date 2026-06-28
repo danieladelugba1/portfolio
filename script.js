ac72ef2f56a4ea588c5c35b0c592a3b8762c78d3
Daniel Adelugba <psyda7@nottingham.ac.uk>
2026-06-28 16:24:14 +0100

    feat: Implmented smooth scrolling for projects cards

--- script.js
+++ script.js
@@ -13,3 +13,25 @@
 // if (localStorage.getItem("darkMode") === "true") {
 //     document.body.classList.add("dark-mode");
 // }
+
+// const list = document.querySelectorAll("#");
+
+// Scrolling rather than jumping for projects cards
+document.querySelectorAll('a[href*="#"]').forEach(link => {
+    // for each link, attach an event listener
+    link.addEventListener('click', function (event) {
+
+        const targetId = this.getAttribute('href').split('#')[1];
+        const target = document.getElementById(targetId);
+
+        if (target) {
+            event.preventDefault(); // prevent jumping
+
+            // smooth scrolling to section
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
